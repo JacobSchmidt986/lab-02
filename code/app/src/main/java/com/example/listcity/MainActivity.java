25f4d62a34f04ac975b5977e979080d009cb6930
JacobSchmidt986 <jacob.schmidt986@gmail.com>
2026-01-16 15:30:17 -0700

    Completed Lab Participation Exercise

--- code/app/src/main/java/com/example/listcity/MainActivity.java
+++ code/app/src/main/java/com/example/listcity/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.listcity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,10 @@ public class MainActivity extends AppCompatActivity {
     ArrayAdapter<String> cityAdapter;
     ArrayList<String> dataList;
 
+    EditText editCity;
+    Button button1,button2;
+    int selectedIndex = -1; //-1 means nothing is selected
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,13 +38,44 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         cityList = findViewById(R.id.city_list);
+        editCity = findViewById(R.id.editCity);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+
 
         String []cities = {"Edmonton", "Vancouver", "Moscow", "Sydney", "Berlin", "Vienna", "Tokyo", "Beijing", "Osaka","New Delhi"};
 
         dataList = new ArrayList<>();
         dataList.addAll(Arrays.asList(cities));
 
-        cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
+        cityAdapter = new ArrayAdapter<>(this, R.layout.content,R.id.content_view, dataList);
         cityList.setAdapter(cityAdapter);
+
+        //single selection + storing index
+        cityList.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        cityList.setOnItemClickListener((parent, view, position, id) -> {
+            selectedIndex = position;
+            cityList.setItemChecked(position, true);
+        });
+        //Add functionality to button 1(Add)
+        button1.setOnClickListener(v -> {
+            String newCity = editCity.getText().toString().trim();
+            if (newCity.isEmpty()){
+                Toast.makeText(this, "Enter City Name", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            dataList.add(newCity);
+            cityAdapter.notifyDataSetChanged();
+            editCity.setText("");
+        });
+        //Add Functionality to button 2(delete)
+        button2.setOnClickListener(v -> {
+           dataList.remove(selectedIndex);
+           cityAdapter.notifyDataSetChanged();
+
+           selectedIndex = -1;
+           cityList.clearChoices();
+        });
+
     }
 }
